1ddac2878c45b42534ce34997fde9f9fdaaafd3e
babucs <m.babufastmail@gmail.com>
2018-02-12 11:19:37 +0530

    Initial commit

---
+++ src/main/java/fmcg/employee/EmployeeTransactionsApplication.java
@@ -0,0 +1,12 @@
+package fmcg.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTransactionsApplication.class, args);
+	}
+}
